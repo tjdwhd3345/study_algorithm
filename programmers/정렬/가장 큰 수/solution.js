c334aa002869f5a43dcbcbddf9961d858ed76999
sungjong <tjdwhd3345@gmail.com>
2021-03-21 21:23:44 +0900

    가장 큰 수

---
+++ programmers/정렬/가장 큰 수/solution.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+  var answer = '';
+  answer = numbers
+    .map((v) => v + '')
+    .sort((a, b) => (b + a) * 1 - (a + b) * 1)
+    .join('');
+
+  return answer[0] === '0' ? '0' : answer;
+}
